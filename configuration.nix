2c63cb62a03ea0dc327c0f3783d4c2996365d360
Axel Magnússon <axel@akzel.net>
2024-08-29 23:20:53 +0200

    actually kinda neat

--- configuration.nix
+++ configuration.nix
@@ -2,28 +2,41 @@
 	"nix-command flakes"; networking.hostName = "Hisoka"; networking.networkmanager.enable = true; boot.loader.systemd-boot.enable = true; 
 	boot.loader.efi.canTouchEfiVariables = true; security.polkit.enable = true;
 
-	#"caps:swapescape";
+#"caps:swapescape";
 
-	services.pipewire = { enable = true; pulse.enable = true;
+	services.pipewire = { enable = true; pulse.enable = true;};
+	services.greetd = {
+		enable = true;
+		settings = {
+			default_session = {
+				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+				user = "greeter";
+			};
 		};
+	};
+
 
 	programs.hyprland.enable = true;
-	
+
 
 	time.timeZone = "Europe/Amsterdam"; i18n.defaultLocale = "en_US.UTF-8"; i18n.extraLocaleSettings = { LC_ADDRESS = "nl_NL.UTF-8"; 
 		LC_IDENTIFICATION = "nl_NL.UTF-8"; LC_MEASUREMENT = "nl_NL.UTF-8"; LC_MONETARY = "nl_NL.UTF-8"; LC_NAME = "nl_NL.UTF-8"; LC_NUMERIC = 
-		"nl_NL.UTF-8"; LC_PAPER = "nl_NL.UTF-8"; LC_TELEPHONE = "nl_NL.UTF-8"; LC_TIME = "nl_NL.UTF-8";
-  		};
-
-	services.gnome.gnome-keyring.enable = true; users.users.axel = { isNormalUser = true; extraGroups = ["wheel" "networkmanager"];
+			"nl_NL.UTF-8"; LC_PAPER = "nl_NL.UTF-8"; LC_TELEPHONE = "nl_NL.UTF-8"; LC_TIME = "nl_NL.UTF-8";
 	};
-	
+
+#services.gnome.gnome-keyring.enable = true; 
+	users.users.axel = { isNormalUser = true; extraGroups = ["wheel" "networkmanager"];
+	};
+
 	environment.systemPackages = with pkgs ; [
 		kitty
 		git
 		qutebrowser
 		gnumake
+		neovim
+		htop
 		neofetch	
-	 ];
-  system.stateVersion = "24.11"; # Did you read the comment?
-}
+		hyprpaper
+	];
+	system.stateVersion = "24.11"; # Did you read the comment?
+			     }
--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 	};
-	outputs = { self, nixpkgs, ...} : {
+	outputs = { self, nixpkgs, ...} @inputs: {
   		nixosConfigurations.Hisoka = nixpkgs.lib.nixosSystem {
+		specialArgs = { inherit inputs; };
 			system = "x86_64-linux";
 			modules = [
 				./configuration.nix	
