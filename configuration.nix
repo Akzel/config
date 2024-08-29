c6c6631fdd56ba03326f297dd07eda2fe8fbdd5b
Axel Magnússon <axel@akzel.net>
2024-08-29 19:36:21 +0200

    aheuifahioe

---
+++ configuration.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... } : {
+	imports = [ ./hardware-configuration.nix ];
+	nixpkgs.config.allowUnfree = true;
+	settings.experimental-features = "nix-command flakes";
+ 	networking.hostName = "Hisoka";
+	networking.networkmanager.enable = true;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	security.polkit.enable = true;
+#x11 
+	services.xserver.enable = true;
+	services.xserver.xkb.layout = "us";
+	services.xserver.xkb.options = "eurosign:e,caps:escape";
+	services.pipewire = {
+		enable = true;
+		pulse.enable = true;
+		};
+
+
+	time.timeZone = "Europe/Amsterdam";
+	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "nl_NL.UTF-8";
+		LC_IDENTIFICATION = "nl_NL.UTF-8";
+    		LC_MEASUREMENT = "nl_NL.UTF-8";
+		LC_MONETARY = "nl_NL.UTF-8";
+		LC_NAME = "nl_NL.UTF-8";
+		LC_NUMERIC = "nl_NL.UTF-8";
+		LC_PAPER = "nl_NL.UTF-8";
+    		LC_TELEPHONE = "nl_NL.UTF-8";
+    		LC_TIME = "nl_NL.UTF-8";
+  		};
+
+	services.gnome.gnome-keyring.enable = true;
+	users.users.axel = {
+		isNormalUser = true;
+		extraGroups = ["wheel" "networkmanager"];
+	};
+	
+	environment.systemPackages = with pkgs ; [
+		grim
+		wl-clipboard
+		mako
+		git
+	];
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
--- flake.nix
+++ flake.nix
@@ -1,31 +1,15 @@
 {
-  description = "Akzel nixos config yuhh";
-
-  inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager"; #/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      Hisoka = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
-      };
-};
-};
+	description = "Akzel nixos config yuhh";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+	outputs = { self, nixpkgs, ...} : {
+  		nixosConfigurations.Hisoka = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				self.realpath ./configuration.nix	
+			];
+		};
+	};
 }
+
