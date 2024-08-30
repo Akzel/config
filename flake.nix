1820774092dcfc6adde6c82b72b6611240e17035
Axel Magnússon <axel@akzel.net>
2024-08-30 16:52:50 +0000

    cursorswap

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
-
   };
+
   outputs = { self, nixpkgs, ...} @inputs:
   {
     nixosConfigurations = {
