9add381706e4a4f994016c6130b5de6a43ae1f6d
Axel Magnússon <axel@akzel.net>
2024-09-29 19:57:25 +0200

    Update lspconfig.lua

--- dots/nvim/lua/plugins/lspconfig.lua
+++ dots/nvim/lua/plugins/lspconfig.lua
@@ -1,34 +1,54 @@
 return {
-	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'hrsh7th/nvim-cmp' },
 	{
-	'lukas-reineke/lsp-format.nvim',
-	config = function()
-		require("lsp-format").setup {}
-	end
+		'lukas-reineke/lsp-format.nvim',
+		config = function()
+			require("lsp-format").setup {}
+		end
 	},
-	{ 
-	'neovim/nvim-lspconfig',
-	config = function ()
-		local lspconfig = require("lspconfig")
-		lspconfig.nil_ls.setup {
-			autostart = true,
-				  on_attach = 	require("lsp-format").on_attach,
-		}
-		lspconfig.lua_ls.setup {
-			on_init = function(client)
-				client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-						workspace = {
-						library = {
-						vim.env.VIMRUNTIME
-						}
-						}
-						})
-			end,
-				on_attach = require("lsp-format").on_attach,
-				settings = {
-					Lua = {}
-				}
-		}
-	end}
+
+	{
+		"neovim/nvim-lspconfig",
+		cmd = 'Mason',
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = {
+			"williamboman/mason.nvim",
+			'hrsh7th/cmp-nvim-lsp',
+			"folke/lazydev.nvim",
+			{ "williamboman/mason-lspconfig.nvim",
+				config = function()
+					require("mason").setup()
+					require("mason-lspconfig").setup()
+					require("mason-lspconfig").setup_handlers {
+						function (server_name)
+							require("lspconfig")[server_name].setup {
+								capabilities = require('cmp_nvim_lsp').default_capabilities(),
+							}
+						end,
+					}
+				end },
+		},
+	},
+	--- lua stuff
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0,
+			})
+		end,
+	},
 }
