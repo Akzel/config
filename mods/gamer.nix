b85407460bd1ababaa76331f3a2d68e9e9cda302
Axel Magnússon <axel@akzel.net>
2024-11-15 18:28:41 +0100

    ok

--- mods/gamer.nix
+++ mods/gamer.nix
@@ -2,7 +2,6 @@
   hardware = {
     graphics.enable = true;
   };
-
   environment.systemPackages = with pkgs; [
     efibootmgr
     mpv
@@ -12,17 +11,5 @@
   ];
   programs.steam = {
     enable = true;
-  };
-  programs.kdeconnect = {
-    enable = true;
-  };
-  networking.firewall = { 
-    enable = true;
-    allowedTCPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-    ];  
-    allowedUDPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-    ];  
   };
 }
